275777833b32cd655acdbbabb38290c7f93f9551
angemonteverde <angemonteverde@gmail.com>
2023-01-20 20:26:03 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = ()  =>  {
+
+let sym = Math.floor(Math.random() *4) ;
+let symbol = ['spade','heart','club','diamond']
+document.querySelector('.card').classList.add(symbol[sym]);
+
+
+
+  let numb = Math.floor(Math.random() *12) ;
+  let number = ["A","2","3","4","5","6","7","8","9","10","J","K","Q"];
+ 
+  document.querySelector('.card').innerHTML = (number[numb]);
+
+}
+
